37915533ffe9832acd1127af828d1a764ff9413f
Andreas <55641247+2020Nonprofit@users.noreply.github.com>
2020-03-24 23:09:30 -0400

    Add files via upload
    This article describes that,“Once these wearables collect enough data to know what your normal baseline readings are, they can get very good at sensing when something’s amiss,” says Snyder. “We think that if your heart rate and skin temperature are elevated for about 2 hours, there’s a strong chance you’re getting sick.” (https://www.newscientist.com/article/2117854-smartwatches-know-youre-getting-a-cold-days-before-you-feel-ill/#ixzz6HfL1tsci) So I implemented a heartrate feature that monitors heartrate in the background and if the heartrate exceeds the average resting heartrate (76 bpm) then a notification is triggered.  Secondly the peer conectivity feature works in the background for an infinite time (so far). If a user is infected and a user was connected the user exposed user is notified.  Next steps: implementing the ui and database!  We ware doing great! :)

--- swift_heart_rate_real_time-master/ElecDemo/APIFetcher.swift
+++ swift_heart_rate_real_time-master/ElecDemo/APIFetcher.swift
@@ -5,31 +5,3 @@
 //  Created by NhatHM on 8/13/19.
 //  Copyright © 2019 GST.PID. All rights reserved.
 //
-
-import Foundation
-
-class APIFetcher {
-    let defaultSession = URLSession(configuration: URLSessionConfiguration.default)
-    var dataTask: URLSessionDataTask?
-    
-    func postHeartRateToServer(url: String, completion: @escaping (String?) -> ()) {
-        
-        let url: URL = URL(string: url)!
-        
-        dataTask = defaultSession.dataTask(with: url) { data, response, error in
-            if let error = error {
-                print(error.localizedDescription)
-                completion(nil)
-            } else if let httpResponse = response as? HTTPURLResponse {
-                if httpResponse.statusCode == 200 {
-                    completion("Sucess get data from server")
-                } else {
-                    print("\(httpResponse.statusCode)")
-                    completion(nil)
-                }
-            }
-        }
-        
-        dataTask?.resume()
-    }
-}
